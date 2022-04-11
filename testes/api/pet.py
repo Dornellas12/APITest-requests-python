febd6aa41a496a465409cb7df5c31c25ca4bb3d5
Brunno Dornelas <dornellas_12@hotmail.com>
2022-04-11 20:24:03 -0300

    Testes de API com Pytest e Requests

--- testes/api/pet.py
+++ testes/api/pet.py
@@ -1,8 +1,24 @@
+import csv
 import pytest  # Engine do teste
 import requests  # Biblioteca para manipular API's (comunicação)
 
 base_url = 'https://petstore.swagger.io/v2'  # Endereço da API (BaseURL)
 headers = {'Content-Type': 'application/json'}  # Informando que os dados serão em Json
+
+def ler_dados_csv():
+    dados_csv = []  # criamos uma lista vazia
+    nome_arquivo = 'C:\\Users\\Particular\\PycharmProjects\\133pets\\vendors\\csv\\pets_positivo.csv'
+    try:
+        with open(nome_arquivo, newline='') as arquivo_csv:
+            campos = csv.reader(arquivo_csv, delimiter=',')
+            next(campos)
+            for linha in campos:
+                dados_csv.append(linha)
+        return dados_csv
+    except FileNotFoundError:
+        print(f'Arquivo não encontrado: {nome_arquivo}')
+    except Exception as fail:
+        print(f'Falha não prevista: {fail}')
 
 class TestApi:
 
@@ -90,7 +106,7 @@ class TestApi:
         assert response_body['type'] == type_esperado
         assert response_body['message'] == message_esperada
 
-    def testar_consultar_pet_deleted(self): # GET (Consultar apos Delete)
+    def testar_consultar_pet_deleted(self):  # GET (Consultar apos Delete)
         # 1. Configura
         pet_id = 410504
         status_code_esperado = 404
@@ -103,3 +119,55 @@ class TestApi:
         print(resultado_obtido)
         corpo_da_resposta = resultado_obtido.json()
         assert resultado_obtido.status_code == status_code_esperado
+
+
+
+@pytest.mark.parametrize('pet_id,category_id,category_name,name,tags_id,tags_name,status,status_code', ler_dados_csv())
+def testar_incluir_pet_json_dinamico(pet_id,category_id,category_name,name,tags_id,tags_name,status,status_code):
+    # 1 - Configura
+    # 1.1 - Dados de Entrada
+    # Utilizará o arquivo pets_positivo.csv
+
+    # 1.2 - Resultados Esperados
+    # Utilizará o arquivo pets_positivo.csv
+
+    # 1.3 - Extra - Montar o json dinamicamicamente a partir do csv
+    corpo_json = '{'
+    corpo_json += f'  "id": {pet_id},'
+    corpo_json += '  "category": {'
+    corpo_json += f'    "id": {category_id},'
+    corpo_json += f'    "name": "{category_name}"'
+    corpo_json += '},'
+    corpo_json += f'"name": "{name}",'
+    corpo_json += '"photoUrls": ['
+    corpo_json += '"string"'
+    corpo_json += '],'
+    corpo_json += '"tags": ['
+    corpo_json += '{'
+    corpo_json += f'    "id": {tags_id},'
+    corpo_json += f'    "name": "{tags_name}"'
+    corpo_json += '}'
+    corpo_json += '],'
+    corpo_json += f'"status": "{status}"'
+    corpo_json += '}'
+
+    print(corpo_json)
+
+    # 2 - Executa
+    resultado_obtido = requests.post(
+        url=base_url+'/pet',
+        data=corpo_json,
+        headers=headers
+    )
+
+    # 3 - Valida
+    assert resultado_obtido.status_code == int(status_code)
+    corpo_da_resposta = resultado_obtido.json()
+    print(corpo_da_resposta)
+    assert corpo_da_resposta['id'] == int(pet_id)
+    assert corpo_da_resposta['category']['id'] == int(category_id)
+    assert corpo_da_resposta['category']['name'] == category_name
+    assert corpo_da_resposta['name'] == name
+    assert corpo_da_resposta['tags'][0]['id'] == int(tags_id)
+    assert corpo_da_resposta['tags'][0]['name'] == tags_name
+    assert corpo_da_resposta['status'] == status
